ef9f251bdfe4551ee4fc961aed78c93ac4d031b4
qaptoR <rocco.ruscitti@outlook.com>
2024-07-15 06:13:02 -0700

    Basic features implemented, documented
    documentation can be improved if necessary

--- lua/fantableous/init.lua
+++ lua/fantableous/init.lua
@@ -97,4 +97,88 @@ function M.move_table_cell_right()
 	-- ts_utils.update_selection(bufnr, node)
 end
 
+function M.move_table_col_left()
+	local node = units.get_table_cell()
+	if not node then
+		return
+	end
+
+	local sibling = node:prev_sibling()
+	if not sibling then
+		return
+	end
+
+	local table = node:parent():parent()
+	if not table then
+		return
+	end
+
+	local index = units.get_row_cell_index(node)
+	local bufnr = vim.api.nvim_get_current_buf()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+
+	local child_count = table:child_count() - 1
+	for i = 0, child_count do
+		local row = table:child(i)
+		if row:type() == "row" then
+			local col_node = row:child(index)
+			if not col_node then
+				return
+			end
+			local other_node = col_node:prev_sibling()
+			if not other_node then
+				return
+			end
+			ts_utils.swap_nodes(col_node, other_node, bufnr, true)
+		end
+	end
+
+	vim.cmd("normal gqgq")
+	vim.api.nvim_win_set_cursor(0, cursor)
+
+	-- ts_utils.update_selection(bufnr, node)
+end
+
+function M.move_table_col_right()
+	local node = units.get_table_cell()
+	if not node then
+		return
+	end
+
+	local sibling = node:next_sibling()
+	if not sibling then
+		return
+	end
+
+	local table = node:parent():parent()
+	if not table then
+		return
+	end
+
+	local index = units.get_row_cell_index(node)
+	local bufnr = vim.api.nvim_get_current_buf()
+	local cursor = vim.api.nvim_win_get_cursor(0)
+
+	local child_count = table:child_count() - 1
+	for i = 0, child_count do
+		local row = table:child(i)
+		if row:type() == "row" then
+			local col_node = row:child(index)
+			if not col_node then
+				return
+			end
+			local other_node = col_node:next_sibling()
+			if not other_node then
+				return
+			end
+			ts_utils.swap_nodes(col_node, other_node, bufnr, true)
+		end
+	end
+
+	vim.cmd("normal gqgq")
+	vim.api.nvim_win_set_cursor(0, cursor)
+
+	-- ts_utils.update_selection(bufnr, node)
+end
+
 return M
